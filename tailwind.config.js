73eb6ceb0480de9760a2b9860bb8747e07edc403
LoraLuo66 <lorae062310@gmail.com>
2024-10-14 17:49:38 +0800

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*', './src/**/*.{html, js, vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
